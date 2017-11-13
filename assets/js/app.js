0bc807de50d1ae1140e986a1c0c30781c32330d1
marcecabelloherrera <marcecabello.h@gmail.com>
2017-11-13 00:01:18 -0300

    segundo commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,48 +1,42 @@
-function add(){
+var btn = document.getElementById('btn');
+boton.addEventListener('click', function(){
+
 	//tenemos que tomar el texto ingresado en el textarea
 	var comments = document.getElementById('comment').value;
-
 	//limpiar el textarea
 	document.getElementById('comment').value = '';
 
 	//contenedor que esta en el html
-	var cont = document.getElementById('cont');
+	var cont = document.getElementById('contador');
 
 	//creamos el div que contiene cada comentario
 	var newComments = document.createElement('div');
+	var elementComment = document.createElement('p');
 
-	//validar que textarea tenga un msje
-	if(comments.length == 0 || comments == null) {
-		alert('Debes ingresar un mensaje');
-		return false;
-	}
-	//crear checkbox
-	var chck = document.createElement('input');
-	chck.type = 'checkbox';
-	//creamos icono de corazon
-	var heart = document.createElement('i');
-	heart.classList.add('fa', 'fa-heart', 'heart');
-	//creamos icono de basura
-	var trash = document.createElement('i');
-	trash.classList.add('fa', 'fa-trash', 'trash');
-	//nodos de texto del textarea
-	var textNewComment = document.createTextNode(comments);
+	//creamos los nodos de texto
+	var nodoTextComment = document.createTextNode(comment);
+	
+	//aca hacemos el appendchild
+	elementComment.appendChild(nodoTextComment);
 
-	var contenedorElemento = document.createElement('p');
-	contenedorElemento.appendChild(textNewComment);
-	newComments.appendChild(chck);
-	newComments.appendChild(trash);
-	newComments.appendChild(heart);
-	newComments.appendChild(contenedorElemento);
+	newComments.appendChild(elementComment);
 
-	cont.appendChild(newComments);
+	//le agregamos como atributo una clase llamada texto
+	newComments.setAttribute('class', 'texto');
 
+	contenedorTexto.appendChild(newComments);
+	var counter = document.getElementById('contenedorTexto');
+	counter.innerHTML = 0;
+})
 
-
-
-
-
-
-
+	var textArea = document.getElementById('comment');
+	textArea.onkeydown = function(){
+	//rescatamos el valor del textarea
+	var comments = document.getElementById('comment').value;
+	//sacamos la longitud de ese mensaje
+	var long = comments.length;
+	//vamos a rescatar
+	var counter = document.getElementById('contador');
+	counter.innerHTML =  long;
 
 }
